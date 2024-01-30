5d241087fee7ae910d90f72f1edda436389b2310
Harrison G <220148717@aston.ac.uk>
2024-01-30 12:05:16 +0000

    complete task 5 using guards

--- lab2-functionalprimer/haskell/func_as_arg.hs
+++ lab2-functionalprimer/haskell/func_as_arg.hs
@@ -1,9 +1,10 @@
 --create inpFunc
 inpFunc a b = [a..b] 
 
---Define applicatorFunc
-applicatorFunc inpFunc a b s = if s=='s' then sum (inpFunc a b) else (sum (inpFunc a b))/(b-a+1)  
+--Define applicatorFunc using guards
+applicatorFunc inpFunc a b s | s == 's' = sum (inpFunc a b)
+                             | s /= 's' = sum(inpFunc a b)/(b-a+1)
 
 main = do
-    let result = applicatorFunc inpFunc 5 12 's'  --Call applicatorFunc with inpFunc and 's' as args
+    let result = applicatorFunc inpFunc 5 12 'a'  --Call applicatorFunc with inpFunc and 's' as args
     putStrLn("sum = " ++ show(result))
